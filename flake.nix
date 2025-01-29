e535e8d662eab5623410f22ac893f476666a0311
Thales Menato <8753631+thamenato@users.noreply.github.com>
2025-01-29 17:42:31 -0500

    chore: update bootdev cli to 1.16.1
    chore: update bootdev version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "K8s Learning Course";
 
   nixConfig = {
-    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-substituters = ["https://nix-community.cachix.org"];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
@@ -13,66 +13,67 @@
     pre-commit-hooks.url = "github:cachix/git-hooks.nix";
   };
 
-  outputs =
-    inputs@{ self, nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+
+    bootdev-cli = pkgs.buildGoModule rec {
+      pname = "bootdev";
+      version = "1.16.1";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "bootdotdev";
+        repo = "bootdev";
+        rev = "v${version}";
+        # To get hash use: `nix flake prefetch github:bootdotdev/bootdev/v<version>`
+        hash = "sha256-e6LUAcG0tCTfRWGkJ85jIfjcr4/1fIP61rPPUTDrkjg=";
       };
 
-      bootdev-cli = pkgs.buildGoModule rec {
-        pname = "bootdev";
-        version = "1.15.1";
+      # Use `pks.lib.fakeHash` the first time when building the module to get
+      # the value of the hash during a version update
+      vendorHash = "sha256-jhRoPXgfntDauInD+F7koCaJlX4XDj+jQSe/uEEYIMM=";
 
-        src = pkgs.fetchFromGitHub {
-          owner = "bootdotdev";
-          repo = "bootdev";
-          rev = "v${version}";
-          # To get hash use: `nix flake prefetch github:bootdotdev/bootdev/v<version>`
-          hash = "sha256-ofXMlH1cvhfCFmgjZVMqt/kF8F9ZlD2CPH55d7dkMN8=";
-        };
-
-        # Use `pks.lib.fakeHash` the first time when building the module to get
-        # the value of the hash during a version update
-        vendorHash = "sha256-jhRoPXgfntDauInD+F7koCaJlX4XDj+jQSe/uEEYIMM=";
-
-        meta = {
-          description = "The official CLI for boot.dev";
-          homepage = "https://github.com/bootdotdev/bootdev";
-        };
-      };
-    in
-    {
-      checks = {
-        pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
-          src = ./.;
-          hooks = {
-            check-added-large-files.enable = true;
-            deadnix.enable = true;
-            end-of-file-fixer.enable = true;
-            nixfmt-rfc-style.enable = true;
-            trim-trailing-whitespace.enable = true;
-          };
-        };
-      };
-
-      devShell."${system}" = pkgs.mkShell {
-        inherit (self.checks.pre-commit-check) shellHook;
-        buildInputs = self.checks.pre-commit-check.enabledPackages;
-
-        packages = with pkgs; [
-          bootdev-cli
-          just
-          k9s
-          kubectl
-          minikube
-          nil
-          nixpkgs-fmt
-          pre-commit
-          uv
-        ];
+      meta = {
+        description = "The official CLI for boot.dev";
+        homepage = "https://github.com/bootdotdev/bootdev";
       };
     };
+  in {
+    checks = {
+      pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          check-added-large-files.enable = true;
+          deadnix.enable = true;
+          end-of-file-fixer.enable = true;
+          alejandra.enable = true;
+          trim-trailing-whitespace.enable = true;
+        };
+      };
+    };
+
+    devShell."${system}" = pkgs.mkShell {
+      inherit (self.checks.pre-commit-check) shellHook;
+      buildInputs = self.checks.pre-commit-check.enabledPackages;
+
+      packages = with pkgs; [
+        bootdev-cli
+        just
+        k9s
+        kubectl
+        minikube
+        nil
+        nixpkgs-fmt
+        pre-commit
+        uv
+      ];
+    };
+  };
 }
